15e66f622b92bbde5b932484e638038c395aadbe
Tallie <84561047+Friendly-Neighbourhood-Insomniac@users.noreply.github.com>
2025-05-26 12:26:17 +0200

    Update welcome screen image path

--- src/ui.js
+++ src/ui.js
@@ -1,11 +1,12 @@
-@@ .. @@
- export function showWelcomeScreen() {
+export function showWelcomeScreen() {
      if (welcomeScreenElement) {
          const welcomeImage = welcomeScreenElement.querySelector('img');
          if (welcomeImage && isMobileDevice()) {
              welcomeImage.src = '/mobileUI.png';
-+        } else {
-+            welcomeImage.src = '/Welcome UI PC.png';
+         } else {
+            welcomeImage.src = '/Welcome UI PC.png';
          }
          welcomeScreenElement.style.opacity = '1';
          welcomeScreenElement.style.display = 'flex';
+     }
+}
